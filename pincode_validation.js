74286189bab88eeed0f55c91b2e92d92d071c64c
Niraj Kumar <nirajgupta7481@gmail.com>
2025-03-04 19:47:06 +0530

    [Niraj] Add . ensured PIN code is exactly 6 digits, ignoring spaces.

--- pincode_validation.js
+++ pincode_validation.js
@@ -31,3 +31,19 @@ console.log(isValidPinUC3("400088"));
 console.log(isValidPinUC3("400 088")); 
 console.log(isValidPinUC3("400088B"));  
 console.log(isValidPinUC3("A400088"));  
+
+
+// UC 4: Ensure PIN code is exactly 6 digits, ignoring spaces
+function isValidPinUC4(pin) {
+    let cleanedPin = pin.replace(/\s/g, "");
+    let pattern = /^[0-9]{6}$/;
+    return pattern.test(cleanedPin);
+}
+
+// test cases
+console.log(isValidPinUC4("400088"));  
+console.log(isValidPinUC4("400 088")); 
+console.log(isValidPinUC4("40008")); 
+console.log(isValidPinUC4("4000888"));  
+console.log(isValidPinUC4("400088B"));  
+console.log(isValidPinUC4("A400088"));  
